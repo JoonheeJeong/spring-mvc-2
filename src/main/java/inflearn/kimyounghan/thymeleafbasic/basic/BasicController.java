f1ef73023da9f901944b334b2c9427d9e62824c1
Joonhee Jeong <jeonggoo75@gmail.com>
2023-07-06 14:41:17 +0900

    implement #1 - thymeleaf link

--- src/main/java/inflearn/kimyounghan/thymeleafbasic/basic/BasicController.java
+++ src/main/java/inflearn/kimyounghan/thymeleafbasic/basic/BasicController.java
@@ -56,6 +56,13 @@ public class BasicController {
         return "basic/objects";
     }
 
+    @GetMapping("link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
 //    @Component
     @Component("helloBean")
     static class HelloBean {
