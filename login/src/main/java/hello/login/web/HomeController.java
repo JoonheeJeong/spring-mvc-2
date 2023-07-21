7ed58916253af5bd1e1811528dc254bb770c993c
Joonhee Jeong <jeonggoo75@gmail.com>
2023-07-21 15:42:36 +0900

    implement #8 - home page with login

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -1,13 +1,39 @@
 package hello.login.web;
 
+import hello.login.web.member.Member;
+import hello.login.web.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+@RequiredArgsConstructor
 @Controller
 public class HomeController {
 
+    private final MemberRepository memberRepository;
+
     @GetMapping("/")
-    public String home() {
-        return "home";
+    public String getHomePage(
+            @CookieValue(required = false) Long memberId,
+            Model model) {
+
+        if (memberId == null) {
+            return "home";
+        }
+
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+
+        return "home-login";
     }
 }
