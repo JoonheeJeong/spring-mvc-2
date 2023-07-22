22c295b9006f6e95af76ea01e6445b7d7586a4bf
Joonhee Jeong <jeonggoo75@gmail.com>
2023-07-22 17:39:47 +0900

    implement #8 - getHomepage using SessionAttribute

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -53,7 +54,7 @@ public class HomeController {
         return "home-login";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String getHomePageServletSession(
             HttpServletRequest request,
             Model model) {
@@ -65,6 +66,19 @@ public class HomeController {
 
         // 세션은 다양한 목적으로 이용되므로, 세션은 존재하지만 데이터가 없을 수 있음. 방어 필수!
         Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "home-login";
+    }
+
+    @GetMapping("/")
+    public String getHomePageSessionAttribute(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
+            Model model) {
+
         if (loginMember == null) {
             return "home";
         }
