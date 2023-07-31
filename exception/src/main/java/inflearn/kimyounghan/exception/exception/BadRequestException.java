505be095bb67209803e525bebde24e95a4ca95b6
Joonhee Jeong <jeonggoo75@gmail.com>
2023-07-31 11:44:44 +0900

    feat: use error message source (#15)

--- exception/src/main/java/inflearn/kimyounghan/exception/exception/BadRequestException.java
+++ exception/src/main/java/inflearn/kimyounghan/exception/exception/BadRequestException.java
@@ -3,6 +3,7 @@ package inflearn.kimyounghan.exception.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "잘못된 사용자 요청입니다.")
+//@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "잘못된 사용자 요청입니다.")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "error.bad")
 public class BadRequestException extends RuntimeException{
 }
